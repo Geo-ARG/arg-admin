99cd2d9477a5376ac468e6f5bb979281904882f5
hacktiv8 <hacktiv8@hacktiv8-4.local>
2017-03-18 22:29:36 +0700

    Break Commit Now, Next Day Again

--- src/components/addevent/index.js
+++ src/components/addevent/index.js
@@ -1,45 +1,66 @@
 import React, {Component} from 'react'
 import '../../App.css'
 import {Grid, Image, Form, Button, TextArea} from 'semantic-ui-react'
+const DatePicker = require('react-datepicker');
+const moment = require('moment');
+
+
+require('react-datepicker/dist/react-datepicker.css');
 
 export default class AddEvent extends Component {
+  constructor(){
+    super()
+    this.state = {
+      startDate: moment()
+    }
+  }
+
+
+
+    handleChange(date) {
+        this.setState({startDate: date});
+    }
+
     render() {
         return (
             <div className='App'>
                 <h1>Game Mangement - New Game</h1>
-                <div>
-                  <Image src='https://4.bp.blogspot.com/-JY1beh0o02M/V3DY9yPdCMI/AAAAAAAAAMI/NQUYgOQdx5MD0_4EQr82nWburZJp14ROwCLcB/s1600/unnamed.png'/>                  
-                </div>
                 <Grid celled>
                     <Grid.Row>
                         <Grid.Column width={10}>
                             <Form>
                                 <Form.Field>
-                                    <label style={{
-                                        float: 'left'
-                                    }}>Geme Event</label>
+                                    <label style={{float: 'left'}}>
+                                      Geme Event
+                                    </label>
                                     <input placeholder='Game Event'/>
                                 </Form.Field>
+                                <label style={{float: 'left'}}>
+                                    <b style={{fontSize: 'small'}}>Game Description</b>
+                                </label>
+                                <Form.Field control={TextArea} placeholder='Tell us more about event'/>
                                 <label style={{
                                     float: 'left'
                                 }}>
                                     <b style={{
                                         fontSize: 'small'
-                                    }}>Game Description</b>
+                                    }}>Date Event</b>
                                 </label>
-                                <Form.Field control={TextArea} placeholder='Tell us more about event'/>
-                                <Form.Field>
-                                    <label style={{
-                                        float: 'left'
-                                    }}>Date Event</label>
-                                    <input placeholder='Date Event'/>
-                                </Form.Field>
-                                <Form.Field>
+                                <br></br>
+                                <div style={{
+                                    float: 'left'
+                                }}>
+                                    <DatePicker selected={this.state.startDate} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <Form.Field style={{
+                                    marginTop: 50
+                                }}>
                                     <label style={{
                                         float: 'left'
                                     }}>Location Event</label>
                                     <input placeholder='Location Event'/>
                                 </Form.Field>
+
                                 <Button positive>Save Game Event</Button>
                             </Form>
                         </Grid.Column>
@@ -47,15 +68,18 @@ export default class AddEvent extends Component {
                             backgroundColor: 'rgb(94, 189, 236)'
                         }}>
                             <p style={{
-                                textAlign: 'left'
+                                textAlign: 'left',
+                                alignItems: 'center'
+
                             }}>
+                                <Image src='https://4.bp.blogspot.com/-JY1beh0o02M/V3DY9yPdCMI/AAAAAAAAAMI/NQUYgOQdx5MD0_4EQr82nWburZJp14ROwCLcB/s1600/unnamed.png'/>
                                 <b>Game Evet :
                                 </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
                             <p style={{
                                 textAlign: 'left'
                             }}>
                                 <b>Game Description :
-                                </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                  </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
                             <p style={{
                                 textAlign: 'left'
                             }}>
