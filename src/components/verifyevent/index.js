8a4d2cbe13ba49f683fb933093556c3bf9a98df0
yonisetiawan <yonisetiawan1990@gmail.com>
2017-03-23 23:53:33 +0700

    midleware verivy image done

--- src/components/verifyevent/index.js
+++ src/components/verifyevent/index.js
@@ -7,7 +7,8 @@ class VerifyEvent extends Component {
     constructor(){
       super()
       this.state={
-        dataConfirmImage: []
+        dataConfirmImage: [],
+        token: false
       }
       this.getListEvent = this.getListEvent.bind(this)
       this.clearAnswerKeyImage = this.clearAnswerKeyImage.bind(this)
@@ -15,6 +16,10 @@ class VerifyEvent extends Component {
     componentWillMount(){
       var token = localStorage.getItem('token')
       this.props.getAllUserEvents(token)
+
+      this.setState({
+          token: localStorage.getItem('token')
+      })
     }
 
 
@@ -33,61 +38,70 @@ class VerifyEvent extends Component {
 
     render() {
       var arrDataEvents = []
-      if(this.props.listUserEvents.length > 0){
-        var hasil = this.props.listUserEvents.map(item=>{
-            return item
-        })
-        arrDataEvents = hasil
+      if(!this.state.token){
+
+      }else{
+        if(this.props.listUserEvents.length > 0){
+          var hasil = this.props.listUserEvents.map(item=>{
+              return item
+          })
+          arrDataEvents = hasil
+        }
       }
+
         return (
           <div className='InputStyle'>
-              <Grid celled>
-                  <Grid.Row>
-                    <Grid.Column width={3}>
-                      <h3>VERIFY IMAGE</h3>
-                      <div className="gameInfo">
-                        <Icon name='game' size='big' />
-                          <b>LIST IMAGES</b>
-                      </div>
-                      <div style={{marginTop: 20}} className="gameInfo">
-                        <Icon name='cancel' size='big' />
-                          <b>Logout</b>
-                      </div>
+            {!this.state.token ? <div></div> :
+              <div>
+                <Grid celled>
+                    <Grid.Row>
+                      <Grid.Column width={3}>
+                        <h3>VERIFY IMAGE</h3>
+                        <div className="gameInfo">
+                          <Icon name='game' size='big' />
+                            <b>LIST IMAGES</b>
+                        </div>
+                        <div style={{marginTop: 20}} className="gameInfo">
+                          <Icon name='cancel' size='big' />
+                            <b>Logout</b>
+                        </div>
 
-                    </Grid.Column>
-                        <Grid.Column width={8}>
-                          <div className='VerifyImage'>
-                            {arrDataEvents.length !== 0 ?
-                              arrDataEvents.map((DataAnswerUser, index)=>{
-                                return (
-                                    <div key={index} style={{width: '20%', marginRight: "2%"}}>
-                                      <Card style={{marginRight:15}}>
-                                            <Image src={DataAnswerUser.userAnswer}/>
-                                              <div style={{marginTop: 5, marginBottom: 5}}>
-                                                <Button onClick={()=>this.getListEvent(DataAnswerUser)} color="orange">Check</Button>
-                                              </div>
-                                      </Card>
-                                    </div>
-                                  )
-                              })
-                            : <h3>List User Answer</h3>}
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column width={5}>
-                            <Card style={{marginRight:15}}>
-                                <Image src={this.state.dataConfirmImage.length !== 0 ? this.state.dataConfirmImage[0].Quest.answerKey : ""}/>
-                                <Card.Content>
-                                    <Card.Header>{this.state.dataConfirmImage.length !== 0 ? this.state.dataConfirmImage[0].Quest.title : ""}</Card.Header>
-                                    <Card.Meta>{this.state.dataConfirmImage.length !== 0 ? this.state.dataConfirmImage[0].Quest.task : "AnswerKey"}</Card.Meta>
-                                </Card.Content>
-                                {this.state.dataConfirmImage.length !== 0 ? <Button onClick={()=>{
-                                  this.props.updateUserCompletion(this.state.dataConfirmImage[0].id, true, localStorage.getItem('token'))
-                                  this.clearAnswerKeyImage()
-                                }} positive>Confirm</Button> : <div></div>}
-                            </Card>
-                        </Grid.Column>
-                  </Grid.Row>
-              </Grid>
+                      </Grid.Column>
+                          <Grid.Column width={8}>
+                            <div className='VerifyImage'>
+                              {arrDataEvents.length !== 0 ?
+                                arrDataEvents.map((DataAnswerUser, index)=>{
+                                  return (
+                                      <div key={index} style={{width: '20%', marginRight: "2%"}}>
+                                        <Card style={{marginRight:15}}>
+                                              <Image src={DataAnswerUser.userAnswer}/>
+                                                <div style={{marginTop: 5, marginBottom: 5}}>
+                                                  <Button onClick={()=>this.getListEvent(DataAnswerUser)} color="orange">Check</Button>
+                                                </div>
+                                        </Card>
+                                      </div>
+                                    )
+                                })
+                              : <h3>List User Answer</h3>}
+                              </div>
+                          </Grid.Column>
+                          <Grid.Column width={5}>
+                              <Card style={{marginRight:15}}>
+                                  <Image src={this.state.dataConfirmImage.length !== 0 ? this.state.dataConfirmImage[0].Quest.answerKey : ""}/>
+                                  <Card.Content>
+                                      <Card.Header>{this.state.dataConfirmImage.length !== 0 ? this.state.dataConfirmImage[0].Quest.title : ""}</Card.Header>
+                                      <Card.Meta>{this.state.dataConfirmImage.length !== 0 ? this.state.dataConfirmImage[0].Quest.task : "AnswerKey"}</Card.Meta>
+                                  </Card.Content>
+                                  {this.state.dataConfirmImage.length !== 0 ? <Button onClick={()=>{
+                                    this.props.updateUserCompletion(this.state.dataConfirmImage[0].id, true, localStorage.getItem('token'))
+                                    this.clearAnswerKeyImage()
+                                  }} positive>Confirm</Button> : <div></div>}
+                              </Card>
+                          </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+              </div>
+            }
           </div>
         )
     }
